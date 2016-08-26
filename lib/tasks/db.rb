82d45a72ca4866b3d255c38cd41786ace082ad74
Pawel Niewiadomski <11110000b@gmail.com>
2016-08-26 18:58:19 +0200

    creating test and development dbs

--- lib/tasks/db.rb
+++ lib/tasks/db.rb
@@ -1,26 +1,20 @@
 class BranchedDatabase
   def self.name(*args)
-    new.name(*args)
+    new(*args).database_name
+  end
+
+  def initialize(prefix: application_name, separator: '_', env: Rails.env)
+    @prefix = prefix
+    @separator = separator
+    @env = env
   end
 
   def current_branch
     `git branch | grep "*"`.chomp.split.last.parameterize
   end
 
-  def existing_databases
-    if ENV['DB_PASSWORD'].present?
-      password = "-p#{ENV['DB_PASSWORD']}"
-    end
-    `mysql -u #{ENV['DB_USER']} #{password} -h #{ENV['DB_HOST']} -e \"show databases\"`.chomp.split
-  end
-
-  def name(prefix: application_name, separator: '_')
-    branch_database = [prefix, current_branch].join(separator)
-    if existing_databases.include?(branch_database)
-      branch_database
-    else
-      [prefix, Rails.env].join(separator)
-    end
+  def database_name
+    [@prefix, current_branch, @env].join(@separator)
   end
 
   def application_name
@@ -33,11 +27,7 @@ namespace :db do
   task :branch => :environment do
     [:development, :test].each do |env|
       config = ActiveRecord::Base.configurations.values_at(env.to_s).first
-
-      if env == :development
-        bdb = BranchedDatabase.new
-        config['database'] = [bdb.application_name, bdb.current_branch].join('_')
-      end
+      config['database'] = BranchedDatabase.name(env: env)
 
       ActiveRecord::Tasks::DatabaseTasks.create(config)
       ActiveRecord::Base.establish_connection(env)
